1e8891593258f59c38e41872bc799c2d58bc319b
Caio Doohan <caiodoohan@Caios-MacBook-Pro-2.local>
2024-02-09 15:49:27 -0300

    Header created

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,9 +7,9 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    fontFamily:{
-      sans: "--font-inter"
-    }
+    fontFamily: {
+      sans: 'var(--font-inter)',
+    },
   },
   plugins: [],
 }
