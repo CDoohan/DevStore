8f9c8c50ccbb3bd890e712299d36ad69e84d9066
Caio Doohan <caiodoohan@Caios-MacBook-Pro-2.local>
2024-02-20 20:40:55 -0300

    Add search api

---
+++ src/app/api/products/search/route.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod'
+import type { NextRequest } from 'next/server'
+import data from '../data.json'
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = request.nextUrl
+
+  const query = z.string().parse(searchParams.get('q'))
+  console.log(query)
+
+  const products = data.products.filter((product) => {
+    return product.title.toLowerCase().includes(query.toLowerCase())
+  })
+
+  return Response.json(products)
+}
