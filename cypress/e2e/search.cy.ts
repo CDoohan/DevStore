a4e253823c31590c650d5de9c2052862b67fd84d
Caio Doohan <caiodoohan@Caios-MacBook-Pro-2.local>
2024-02-26 14:49:11 -0300

    Add cypress e2e tests

---
+++ cypress/e2e/search.cy.ts
@@ -0,0 +1,19 @@
+describe('search products', () => {
+  /// Testing search navigate
+  it('should be able to search for a product and add it to the cart', () => {
+    cy.searchByQuery('moletom')
+
+    cy.location('pathname').should('include', '/search')
+    cy.location('search').should('include', 'q=moletom')
+
+    cy.get('a[href^="/product"]').should('exist')
+  })
+
+  it('should not be able to visit search page without a search query', () => {
+    cy.on('uncaught:exception', () => {
+      return false
+    })
+    cy.visit('/search')
+    cy.location('pathname').should('equal', '/')
+  })
+})
